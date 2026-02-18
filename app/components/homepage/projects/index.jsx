84fce32ebd4bb82594802e11e72968de86d890e4
MyTution <rohitsonawane889@gmail.com>
2026-02-18 23:50:55 +0530

    cas

--- app/components/homepage/projects/index.jsx
+++ app/components/homepage/projects/index.jsx
@@ -1,15 +1,16 @@
+"use client";
+
 import { projectsData } from '@/utils/data/projects-data';
 import ProjectCard from './project-card';
-import VideoCarousel from './video-slider';
-import { videos } from '@/utils/data/videos';
+
 const Projects = () => {
 
   return (
-    <div id='projects' className="relative z-50  my-12 lg:my-24">
+    <div id='projects' className="relative z-50 my-12 lg:my-24">
       <div className="sticky top-10">
-        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
+        <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl opacity-30"></div>
         <div className="flex items-center justify-start relative">
-          <span className="bg-[#2F2F2F] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
+          <span className="bg-[#2F2F2F] absolute left-0 w-fit text-white px-5 py-3 text-xl rounded-md">
             PROJECTS
           </span>
           <span className="w-full h-[2px] bg-[#2F2F2F]"></span>
@@ -32,18 +33,9 @@ const Projects = () => {
         </div>
       </div>
 
-      {/* <div className="w-[80px] h-[80px] bg-violet-100 rounded-full absolute -top-3 left-0 translate-x-1/2 filter blur-3xl  opacity-30"></div>
-      <div className="flex items-center justify-start relative">
-          <span className="bg-[#2F2F2F] absolute left-0  w-fit text-white px-5 py-3 text-xl rounded-md">
-            PROJECT DEMO
-          </span>
-        </div>
-         */}
-          {/* <span className="w-full h-[2px] bg-[#2F2F2F]"></span> */}
+      {/* If you enable this later, it also needs client rendering */}
+      {/* <VideoCarousel videosData={videos} /> */}
 
-      {/* <div>
-      <VideoCarousel videosData={videos} />
-    </div> */}
     </div>
   );
 };
