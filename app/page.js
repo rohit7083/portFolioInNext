858ccea8d818b72d8cceca324a5ec8a1e13f3420
MyTution <rohitsonawane889@gmail.com>
2026-02-19 14:03:23 +0530

    ds

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@
 import { personalData } from "@/utils/data/personal-data";
 import dynamic from "next/dynamic";
 
-const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
-const Blog = dynamic(() => import("./components/homepage/blog"), { ssr: false });
-const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
-const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
-const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
-const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
-const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
-const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
+const AboutSection = dynamic(() => import("./components/homepage/about"));
+const Blog = dynamic(() => import("./components/homepage/blog"));
+const ContactSection = dynamic(() => import("./components/homepage/contact"));
+const Education = dynamic(() => import("./components/homepage/education"));
+const Experience = dynamic(() => import("./components/homepage/experience"));
+const HeroSection = dynamic(() => import("./components/homepage/hero-section"));
+const Projects = dynamic(() => import("./components/homepage/projects"));
+const Skills = dynamic(() => import("./components/homepage/skills"));
 
 async function getData() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
